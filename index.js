13103c5a2fee856fd5c783f18eed121e0bf0fa03
JfRomo <jfromo98@gmail.com>
2022-04-20 15:59:58 -0500

    Ahora si

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const routerApi = require('./routes/index')
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hola!, muy buenas');
+})
+
+app.get('/home', (req, res) => {
+  res.send('Bienvenido a este sitio web');
+})
+
+app.listen(port, () => {
+  console.log('Escuchanding en el puerto ' + port);
+})
+
+routerApi(app);
